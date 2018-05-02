686abf9e94bf9768a23ca29418759df68d9b004b
Erekata Tega <tega23@yahoo.com>
2018-05-02 21:40:52 +0100

    View items in inventory

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order'
 import Inventory from './Inventory'
 import SampleFishes from '../sample-fishes'
 import Fish from './Fish'
+import base from '../base'
 
 class App extends React.Component{
     constructor(){
@@ -11,15 +12,47 @@ class App extends React.Component{
         this.addFish = this.addFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
+        this.updateFish = this.updateFish.bind(this);
+        this.removeFish = this.removeFish.bind(fish);
         this.state = {
             fishes:{},
             order:{},
         }
     }
+    componentWillMount(){
+        //runs before the <App/> is rendereed
+        this.ref = base.syncState(`${this.props.params.storeid}/fishes/`,{context:this, state:'fishes'});
+        //checks if there is any order in localStorage
+        const localStorageRef = localStorage.getItem(`order-${this.props.params.storeid}`)
+        if(localStorageRef){
+            //update our <App/> component's order state
+            this.setState(
+                { order : JSON.parse(localStorageRef)}
+            )
+        }
+    }
+    componentWillUpdate(nextProps, nextState){
+        localStorage.setItem(`order-${this.props.params.storeid}`, JSON.stringify(nextState.order))
+    }
+    componentWIllUnMount(){  
+        base.removeBinding(this.ref);
+    }
     addFish(fish){
         const fishes = {...this.state.fishes}
         const timestamp = Date.now()
         fishes[`fish-${timestamp}`] = fish
+        this.setState({fishes})
+    }
+    updateFish(key , updatedFish){
+        const fishes = {...this.state.fishes}
+        console.log("Before" +fishes[key].status)
+        fishes[key] = updatedFish
+        this.setState({fishes})
+        console.log("After" +this.state.fishes[key].status)
+    }
+    removeFish(key){
+        const fishes ={...this.state.fishes}
+        fishes[key] = null;
         this.setState({fishes})
     }
     loadSamples(){
@@ -40,7 +73,12 @@ class App extends React.Component{
                     </ul>
                 </div>
                 <Order fishes={this.state.fishes} order={this.state.order} />
-                <Inventory addFish={this.addFish}  loadSamples = {this.loadSamples}/>
+                <Inventory 
+                    addFish={this.addFish} 
+                    fishes={this.state.fishes} 
+                    loadSamples = {this.loadSamples}
+                    updateFish={this.updateFish}
+                />
             </div>
         )
     }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import AddFishForm from './AddFishForm'
 class Inventory extends React.Component{
+    constructor(){
+        super()
+        this.renderInventory = this.renderInventory.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event , key){
+        const fish = this.props.fishes[key]
+        const updatedFish ={...fish , [event.target.name]:event.target.value}
+        this.props.updateFish(key , updatedFish)
+    }
+    renderInventory(key){
+        const fish = this.props.fishes[key]
+
+        return(
+            <div className="fish-edit" key={key}>
+                <input name="name" type="text" placeholder='Fish Name' value={fish.name} onChange={(e)=>this.handleChange(e,key)} />
+                <input name="price" type="text" placeholder='Fish Price' value={fish.price} onChange={(e)=>this.handleChange(e,key)} />
+                <select name="status" value={fish.status} onChange={(e)=>this.handleChange(e,key)}>
+                    <option value='available'>Fresh</option>
+                    <option value='unavailable'>Sold Out!</option>
+                </select>
+                <textarea name="desc" placeholder="Fish Desc..." value={fish.desc} onChange={(e)=>this.handleChange(e,key)}></textarea>
+                <input name="image" type="text" placeholder='Fish Image' value={fish.image} onChange={(e)=>this.handleChange(e,key)}/>
+            </div>
+        )
+    }
     render(){
         return (
             <div>
                 <h3>Inventory</h3>
+                {Object.keys(this.props.fishes).map(this.renderInventory)}
                 <AddFishForm addFish={this.props.addFish}/>
                 <button  onClick ={this.props.loadSamples}>Load sample fishes</button>
             </div>
