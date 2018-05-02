7b5b154cb25c60ce875de3bafa4f30d947e78fea
Erekata Tega <tega23@yahoo.com>
2018-05-02 11:05:43 +0100

    Fuctionality to add orders

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component{
         super()
         this.addFish = this.addFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
+        this.addToOrder = this.addToOrder.bind(this);
         this.state = {
             fishes:{},
             order:{},
@@ -24,16 +25,21 @@ class App extends React.Component{
     loadSamples(){
         this.setState({fishes: SampleFishes})
     }
+    addToOrder(key){
+        const order = {...this.state.order}
+        order[key] = order[key] + 1 || 1;
+        this.setState({order})
+    }
     render(){
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh sea food market"/>
                     <ul className="list-of-fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/> )}
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} index={key} addToOrder={this.addToOrder}/> )}
                     </ul>
                 </div>
-                <Order/>
+                <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory addFish={this.addFish}  loadSamples = {this.loadSamples}/>
             </div>
         )
